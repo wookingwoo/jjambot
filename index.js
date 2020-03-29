24e162465615e83d1a4b283372ad7339e1c0c3d0
JeongHeon Woo <email@woojeongheon.com>
2020-03-29 11:00:26 +0000

    made index.js (skill server)
    add .gitignore for keyfile
    
    installed module xml-js request
    
    Signed-off-by: JeongHeon Woo <email@woojeongheon.com>

---
+++ index.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const apiRouter = express.Router();
+
+app.use(logger('dev', {}));
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+app.use('/api', apiRouter);
+
+apiRouter.post('/sayHello', function(req, res) {
+    const responseBody = {
+        version: '2.0',
+        template: {
+            outputs: [
+                {
+                    simpleText: {
+                        text: "hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    };
+
+    res.status(200).send(responseBody);
+});
+
+apiRouter.post('/showHello', function(req, res) {
+    console.log(req.body);
+
+    const responseBody = {
+        version: '2.0',
+        template: {
+            outputs: [
+                {
+                    simpleImage: {
+                        imageUrl:
+                            'https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg',
+                        altText: "hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    };
+
+    res.status(200).send(responseBody);
+});
+
+apiRouter.post('/datatest', function(req, res) {
+    const responseBody = {
+        version: '2.0',
+        data: {
+            msg: 'HI',
+            name: 'Ryan',
+            position: 'Senior Managing Director'
+        }
+    };
+
+    res.status(200).send(responseBody);
+});
+
+apiRouter.post('/menu', function(req, res) {
+    console.log(req.body);
+
+    const convert = require('xml-js'); // npm install xml-js request
+
+    const request = require('request');
+
+    var module_api_key = require('./api_key');
+    const HOST = module_api_key.HOST;
+    const KEY = module_api_key.KEY;
+    const TYPE = module_api_key.TYPE;
+    const SERVICE = module_api_key.SERVICE;
+    const START_INDEX = module_api_key.START_INDEX;
+    const END_INDEX = module_api_key.END_INDEX;
+
+    var requestUrl = `${HOST}/${KEY}/${TYPE}/${SERVICE}/${START_INDEX}/${END_INDEX}`;
+
+    var xmlToJson;
+    var response_menu;
+    var jsonData;
+
+    request.get(requestUrl, (err, res, body) => {
+        if (err) {
+            console.log(`err => ${err}`);
+        } else {
+            if (res.statusCode == 200) {
+                var result = body;
+
+                // console.log(`body data => ${result}`)
+
+                xmlToJson = convert.xml2json(result, { compact: true, spaces: 4 }); // compact(데이터 간소화 여부), spaces(들여쓰기 포인트)
+
+                console.log(`xmlToJson 타입 => ${typeof xmlToJson}`);
+
+                // console.log(`xml to json => ${xmlToJson}`)
+
+                jsonData = JSON.parse(xmlToJson);
+                console.log(`jsonData 타입 => ${typeof jsonData}`);
+
+                response_menu = jsonData.DS_TB_MNDT_DATEBYMLSVC_5322.row[0];
+                console.log(`response_menu 타입 => ${typeof response_menu}`);
+
+                console.log(`json 출력값 => ${JSON.stringify(response_menu)}`);
+            }
+        }
+    });
+
+    setTimeout(function() {
+        console.log('===========');
+        console.log(`json 출력값 => ${JSON.stringify(response_menu)}`);
+        console.log('===========');
+
+        const responseBody = {
+            version: '2.0',
+            data: {
+                msg: 'HI',
+                name: 'Ryan',
+                position: 'Senior Managing Director',
+                menu: response_menu
+            }
+        };
+
+        res.status(200).send(responseBody);
+    }, 3000);
+});
+
+app.listen(3000, function() {
+    console.log('jjambot menu skill server listening on port 3000!');
+});
