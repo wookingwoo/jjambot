73a0cc9f217b50940de1ecb788699575ec4a5f5f
JeongHeon Woo <email@woojeongheon.com>
2020-04-03 16:10:33 +0000

    made all_corps_menu working
    Signed-off-by: JeongHeon Woo <email@woojeongheon.com>

--- crawler/main.py
+++ crawler/main.py
@@ -3,44 +3,53 @@ from api_key import *
 from bs4 import BeautifulSoup
 import requests
 
-info_url = HOST + "/" + KEY + "/" + TYPE + "/" + SERVICE + "/" + START_INDEX + "/" + END_INDEX
-response = requests.get(info_url)
-soup = BeautifulSoup(response.content, 'html.parser')
+all_corps_menu = {}
 
-menu = {}
-date = ""
+for i in range(len(corps)):
+    print()
+    print("corps:", corps[i])
 
-rows = soup.find_all('row')
+    response = requests.get(info_url[i])
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-for row in rows:
-    if not (row.find('dates').text == ""):
+    menu = {}
+    date = ""
 
-        if not (date == ""):
-            menu[date] = {"breakfast": breakfast, "lunch": lunch, "dinner": dinner, "specialFood": specialFood}
-        # menu = {날짜:{아침:[], 점심:[], 저녁:[], 부식[]}}
+    rows = soup.find_all('row')
 
-        date = row.find('dates').text
-        print("-----", date, "-----")
-        breakfast = []
-        lunch = []
-        dinner = []
-        specialFood = []
+    for row in rows:
+        if not (row.find('dates').text == ""):
 
-    if not (row.find('brst').text == ""):
-        print(date, "(아침):", row.find('brst').text)
-        breakfast.append(row.find('brst').text)
+            if not (date == ""):
+                menu[date] = {"breakfast": breakfast, "lunch": lunch, "dinner": dinner, "specialFood": specialFood}
+            # menu = {날짜:{아침:[], 점심:[], 저녁:[], 부식[]}}
 
-    if not (row.find('lunc').text == ""):
-        print(date, "(점심):", row.find('lunc').text)
-        lunch.append(row.find('lunc').text)
+            date = row.find('dates').text
+            print("-----", date, "-----")
+            breakfast = []
+            lunch = []
+            dinner = []
+            specialFood = []
 
-    if not (row.find('dinr').text == ""):
-        print(date, "(저녁):", row.find('dinr').text)
-        dinner.append(row.find('dinr').text)
+        if not (row.find('brst').text == ""):
+            print(date, "(아침):", row.find('brst').text)
+            breakfast.append(row.find('brst').text)
 
-    if not (row.find('adspcfd').text == ""):
-        print(date, "(부식):", row.find('adspcfd').text)
-        specialFood.append(row.find('adspcfd').text)
+        if not (row.find('lunc').text == ""):
+            print(date, "(점심):", row.find('lunc').text)
+            lunch.append(row.find('lunc').text)
 
-print()
-print(menu)
+        if not (row.find('dinr').text == ""):
+            print(date, "(저녁):", row.find('dinr').text)
+            dinner.append(row.find('dinr').text)
+
+        if not (row.find('adspcfd').text == ""):
+            print(date, "(부식):", row.find('adspcfd').text)
+            specialFood.append(row.find('adspcfd').text)
+
+    print()
+    print(menu)
+
+    all_corps_menu[corps[i]] = menu
+
+print("끝")
