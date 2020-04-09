5504aa531cbf821807210344715d6847b8a2a58e
JeongHeon Woo <email@woojeongheon.com>
2020-04-09 14:55:07 +0000

    add new def(def writeMenuAsDate_TXT)
    Signed-off-by: JeongHeon Woo <email@woojeongheon.com>

--- crawler/write_menu_data.py
+++ crawler/write_menu_data.py
@@ -3,3 +3,12 @@ def writeAllCorpsMenu_TXT(dic):
     f.write(str(dic))
     f.close()
     print("./crawling_data/allCorpsMenu.txt에 파일 쓰기 완료.")
+
+
+def writeMenuAsDate_TXT(dic):
+    # f = open("./crawling_data/allCorpsMenu.txt", 'w')
+    # f.write(str(dic))
+    # f.close()
+    # print("./crawling_data/allCorpsMenu.txt에 파일 쓰기 완료.")
+
+    pass
